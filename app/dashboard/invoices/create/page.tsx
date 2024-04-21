b94bdcc8c49e2597dc8320fb10ce66c103767913
Matheus Rafael <matheusrafael77@hotmail.com>
2024-04-21 14:36:48 -0300

    Corrigir estilos de classe em breadcrumbs.tsx, buttons.tsx, create-form.tsx e edit-form.tsx

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,23 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() { // isso é um componente executado no servidor por que é uma função async
+  const customers = await fetchCustomers(); // resgate de dados de clientes da API
+ 
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} /> 
+    </main>
+  );
+}
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,10 +7,12 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  // isso é um componente executado no cliente por que é uma função normal
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
